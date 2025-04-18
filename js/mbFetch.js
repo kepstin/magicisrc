7f490a143cb129aa834184826b5ccba2804c2fb2
Calvin Walton <calvin.walton@kepstin.ca>
2025-04-18 14:50:02 -0400

    Fix building ISRC metadata for releases without pregap.

--- js/mbFetch.js
+++ js/mbFetch.js
@@ -1,3 +1,4 @@
+// @ts-check
 /*
  * Copyright © 2025 Calvin Walton
  *
@@ -16,8 +17,6 @@
  * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-
-// @ts-check
 
 /**
  * @typedef {Object} RateControl
